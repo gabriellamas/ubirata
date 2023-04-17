0e5e9b1d5a0a7955e4cf034855ede888a5631b83
Gabriel Lamas <gabrielldarts@gmail.com>
2023-04-17 17:52:32 -0300

    correct lat-long

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -1,23 +1,25 @@
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import { MapContainer, Polygon, TileLayer } from 'react-leaflet'
+import BairrosFakeApi from '../../fake-bairros-api.json'
 
 const Map = () => {
-  const position: [number, number] = [51.505, -0.09]
-
   return (
     <MapContainer
-      center={position}
-      zoom={13}
+      center={[-23.198917, -45.905913]}
+      zoom={14}
       style={{ width: '100%', height: '100%' }}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={position}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+
+      {BairrosFakeApi.features.map((bairro) => {
+        const myCoordinates: any = bairro.geometry.coordinates
+          .flat(2)
+          .map((latLong) => latLong.reverse())
+        // eslint-disable-next-line react/jsx-key
+        return <Polygon positions={myCoordinates} />
+      })}
     </MapContainer>
   )
 }
---
+++ src/components/map/map.test.tsx
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react'
+import Map from '.'
+
+describe('Map', () => {
+  it('should render map', () => {
+    render(<Map />)
+  })
+})
