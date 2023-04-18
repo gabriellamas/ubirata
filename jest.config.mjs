8e16123e677ceb825390cb47a2d0a285447bd5fa
Gabriel Lamas <gabrielldarts@gmail.com>
2023-04-18 16:01:14 -0300

    change test index name

--- jest.config.mjs
+++ jest.config.mjs
@@ -11,8 +11,7 @@ const createJestConfig = nextJest({
 const config = {
   // Add more setup options before each test is run
   testEnvironment: 'jest-environment-jsdom',
-  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
-  setupFilesAfterEnv: ['./src/__setup__/setupTests.js']
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
